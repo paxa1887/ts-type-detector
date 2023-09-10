a56fad46f4ac9b8843987822eba4c1668fb7f18b
paxa1887 <paxa1887@gmail.com>
2023-09-10 18:50:10 +0200

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,143 @@
-export { type_detector } from './type_detector';
+export function whoami(val: any): string {
+  if (val === void 0) return 'undefined';
+  if (val === null) return 'null';
+
+  const type = typeof val;
+
+  if (type === 'boolean') return 'boolean';
+  if (type === 'string') return 'string';
+  if (type === 'number') return 'number';
+  if (type === 'symbol') return 'symbol';
+  if (type === 'function') {
+    return isGeneratorFn(val) ? 'generatorfunction' : 'function';
+  }
+  if (isArray(val)) return 'array';
+  if (isBuffer(val)) return 'buffer';
+  if (isArguments(val)) return 'arguments';
+  if (isDate(val)) return 'date';
+  if (isError(val)) return 'error';
+  if (isRegexp(val)) return 'regexp';
+
+  switch (ctorName(val)) {
+    case 'Symbol':
+      return 'symbol';
+    case 'Promise':
+      return 'promise';
+
+    case 'WeakMap':
+      return 'weakmap';
+    case 'WeakSet':
+      return 'weakset';
+    case 'Map':
+      return 'map';
+    case 'Set':
+      return 'set';
+
+    case 'Int8Array':
+      return 'int8array';
+    case 'Uint8Array':
+      return 'uint8array';
+    case 'Uint8ClampedArray':
+      return 'uint8clampedarray';
+
+    case 'Int16Array':
+      return 'int16array';
+    case 'Uint16Array':
+      return 'uint16array';
+
+    case 'Int32Array':
+      return 'int32array';
+    case 'Uint32Array':
+      return 'uint32array';
+    case 'Float32Array':
+      return 'float32array';
+    case 'Float64Array':
+      return 'float64array';
+  }
+
+  if (isGeneratorObj(val)) {
+    return 'generator';
+  }
+
+  const objectType = toString.call(val) as string;
+  switch (objectType) {
+    case '[object Object]':
+      return 'object';
+    // iterators
+    case '[object Map Iterator]':
+      return 'mapiterator';
+    case '[object Set Iterator]':
+      return 'setiterator';
+    case '[object String Iterator]':
+      return 'stringiterator';
+    case '[object Array Iterator]':
+      return 'arrayiterator';
+  }
+
+  return type.slice(8, -1).toLowerCase().replace(/\s/g, '');
+}
+
+function ctorName(val: any): string | null {
+  return typeof val.constructor === 'function' ? val.constructor.name : null;
+}
+
+function isArray(val: any): boolean {
+  if (Array.isArray) return Array.isArray(val);
+  return val instanceof Array;
+}
+
+function isError(val: any): boolean {
+  return (
+    val instanceof Error ||
+    (typeof val.message === 'string' && val.constructor && typeof val.constructor.stackTraceLimit === 'number')
+  );
+}
+
+function isDate(val: any): boolean {
+  if (val instanceof Date) return true;
+  return (
+    typeof val.toDateString === 'function' && typeof val.getDate === 'function' && typeof val.setDate === 'function'
+  );
+}
+
+function isRegexp(val: any): boolean {
+  if (val instanceof RegExp) return true;
+  return (
+    typeof val.flags === 'string' &&
+    typeof val.ignoreCase === 'boolean' &&
+    typeof val.multiline === 'boolean' &&
+    typeof val.global === 'boolean'
+  );
+}
+
+function isGeneratorFn(name: any): boolean {
+  return ctorName(name) === 'GeneratorFunction';
+}
+
+function isGeneratorObj(val: any): boolean {
+  return typeof val.throw === 'function' && typeof val.return === 'function' && typeof val.next === 'function';
+}
+
+function isArguments(val: any): boolean {
+  try {
+    if (typeof val.length === 'number' && typeof val.callee === 'function') {
+      return true;
+    }
+  } catch (err) {
+    if (
+      err instanceof Error &&
+      typeof (err as Error).message === 'string' &&
+      (err as Error).message.includes('callee')
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isBuffer(val: any): boolean {
+  if (val.constructor && typeof val.constructor.isBuffer === 'function') {
+    return val.constructor.isBuffer(val);
+  }
+  return false;
+}
--- tests/whoami.test.ts
+++ tests/whoami.test.ts
@@ -1,34 +1,34 @@
 import { describe, expect } from '@jest/globals';
-import { type_detector } from '../src/type_detector';
+import { whoami } from '../src/index';
 
 describe('Add function', function () {
   describe('type_detector', function () {
     describe('null and undefined', function () {
       it('should work for undefined', function () {
-        expect(type_detector(undefined)).toBe('undefined');
+        expect(whoami(undefined)).toBe('undefined');
       });
       it('should work for null', function () {
-        expect(type_detector(null)).toBe('null');
+        expect(whoami(null)).toBe('null');
       });
     });
     describe('primitives', function () {
       it('should work for booleans', function () {
-        expect(type_detector(true)).toBe('boolean');
-        expect(type_detector(false)).toBe('boolean');
+        expect(whoami(true)).toBe('boolean');
+        expect(whoami(false)).toBe('boolean');
       });
 
       it('should work for numbers', function () {
-        expect(type_detector(42)).toBe('number');
+        expect(whoami(42)).toBe('number');
       });
 
       it('should work for strings', function () {
-        expect(type_detector('str')).toBe('string');
+        expect(whoami('str')).toBe('string');
       });
     });
     describe('objects', function () {
       it('should work for arguments', function () {
         (() => {
-          expect(type_detector(arguments)).toBe('arguments');
+          expect(whoami(arguments)).toBe('arguments');
         })();
       });
 
@@ -41,162 +41,162 @@ describe('Add function', function () {
         const createdNull = Object.create(null);
         const createdObj = Object.create({});
 
-        expect(type_detector(instance)).toBe('object');
-        expect(type_detector(literal)).toBe('object');
-        expect(type_detector(createdNull)).toBe('object');
-        expect(type_detector(createdObj)).toBe('object');
+        expect(whoami(instance)).toBe('object');
+        expect(whoami(literal)).toBe('object');
+        expect(whoami(createdNull)).toBe('object');
+        expect(whoami(createdObj)).toBe('object');
       });
 
       it('should work for buffers', function () {
-        expect(type_detector(new Buffer(''))).toBe('buffer');
+        expect(whoami(new Buffer(''))).toBe('buffer');
       });
 
       it('should work for dates', function () {
-        expect(type_detector(new Date())).toBe('date');
+        expect(whoami(new Date())).toBe('date');
       });
 
       it('should work for arrays', function () {
-        expect(type_detector([])).toBe('array');
-        expect(type_detector([1, 2, 3])).toBe('array');
-        expect(type_detector([])).toBe('array');
+        expect(whoami([])).toBe('array');
+        expect(whoami([1, 2, 3])).toBe('array');
+        expect(whoami([])).toBe('array');
       });
 
       it('should work for regular expressions', function () {
-        expect(type_detector(/./)).toBe('regexp');
-        expect(type_detector(new RegExp('^foo$'))).toBe('regexp');
+        expect(whoami(/./)).toBe('regexp');
+        expect(whoami(new RegExp('^foo$'))).toBe('regexp');
       });
 
       it('should work for functions', function () {
-        expect(type_detector(function () {})).toBe('function');
-        expect(type_detector(new Function())).toBe('function');
+        expect(whoami(function () {})).toBe('function');
+        expect(whoami(new Function())).toBe('function');
       });
 
       it('should work for Errors', function () {
-        expect(type_detector(new Error(''))).toBe('error');
+        expect(whoami(new Error(''))).toBe('error');
       });
     });
     describe('es6 features', function () {
       it('should work for resolved promises', function () {
         const promise = Promise.resolve(123);
-        expect(type_detector(promise)).toBe('promise');
+        expect(whoami(promise)).toBe('promise');
       });
 
       it('should work for rejected promises', function () {
         const promise = Promise.reject(new Error('foo bar'));
         promise.catch(function () {});
-        expect(type_detector(promise)).toBe('promise');
+        expect(whoami(promise)).toBe('promise');
       });
 
       it('should work for generator functions', function () {
         const gen = function* named() {
           yield true;
         };
-        expect(type_detector(gen)).toBe('generatorfunction');
+        expect(whoami(gen)).toBe('generatorfunction');
       });
 
       it('should work for generator objects', function () {
         const gen = function* named() {
           yield true;
         };
-        expect(type_detector(gen())).toBe('generator');
+        expect(whoami(gen())).toBe('generator');
       });
 
       it('should work for template strings', function () {
         const name = 'Foo';
-        expect(type_detector(`Welcome ${name} buddy`)).toBe('string');
+        expect(whoami(`Welcome ${name} buddy`)).toBe('string');
       });
 
       it('should work for Map', function () {
         const map = new Map();
-        expect(type_detector(map)).toBe('map');
-        expect(type_detector(map.set)).toBe('function');
-        expect(type_detector(map.get)).toBe('function');
+        expect(whoami(map)).toBe('map');
+        expect(whoami(map.set)).toBe('function');
+        expect(whoami(map.get)).toBe('function');
       });
 
       it('should work for WeakMap', function () {
         const weakmap = new WeakMap();
-        expect(type_detector(weakmap)).toBe('weakmap');
-        expect(type_detector(weakmap.set)).toBe('function');
-        expect(type_detector(weakmap.get)).toBe('function');
+        expect(whoami(weakmap)).toBe('weakmap');
+        expect(whoami(weakmap.set)).toBe('function');
+        expect(whoami(weakmap.get)).toBe('function');
       });
 
       it('should work for Set', function () {
         const set = new Set();
-        expect(type_detector(set)).toBe('set');
-        expect(type_detector(set.add)).toBe('function');
+        expect(whoami(set)).toBe('set');
+        expect(whoami(set.add)).toBe('function');
       });
 
       it('should work for WeakSet', function () {
         const weakset = new WeakSet();
-        expect(type_detector(weakset)).toBe('weakset');
-        expect(type_detector(weakset.add)).toBe('function');
+        expect(whoami(weakset)).toBe('weakset');
+        expect(whoami(weakset.add)).toBe('function');
       });
 
       it('should work for Set Iterator', function () {
         const SetValuesIterator = new Set().values();
-        expect(type_detector(SetValuesIterator)).toBe('setiterator');
+        expect(whoami(SetValuesIterator)).toBe('setiterator');
       });
       it('should work for Map Iterator', function () {
         const MapValuesIterator = new Map().values();
-        expect(type_detector(MapValuesIterator)).toBe('mapiterator');
+        expect(whoami(MapValuesIterator)).toBe('mapiterator');
       });
       it('should work for Array Iterator', function () {
         const ArrayEntriesIterator = [].entries();
-        expect(type_detector(ArrayEntriesIterator)).toBe('arrayiterator');
+        expect(whoami(ArrayEntriesIterator)).toBe('arrayiterator');
       });
       it('should work for String Iterator', function () {
         const StringCharIterator = ''[Symbol.iterator]();
-        expect(type_detector(StringCharIterator)).toBe('stringiterator');
+        expect(whoami(StringCharIterator)).toBe('stringiterator');
       });
 
       it('should work for Symbol', function () {
-        expect(type_detector(Symbol('foo'))).toBe('symbol');
-        expect(type_detector(Symbol.prototype)).toBe('symbol');
+        expect(whoami(Symbol('foo'))).toBe('symbol');
+        expect(whoami(Symbol.prototype)).toBe('symbol');
       });
 
       it('should work for Int8Array', function () {
         const int8array = new Int8Array();
-        expect(type_detector(int8array)).toBe('int8array');
+        expect(whoami(int8array)).toBe('int8array');
       });
 
       it('should work for Uint8Array', function () {
         const uint8array = new Uint8Array();
-        expect(type_detector(uint8array)).toBe('uint8array');
+        expect(whoami(uint8array)).toBe('uint8array');
       });
 
       it('should work for Uint8ClampedArray', function () {
         const uint8clampedarray = new Uint8ClampedArray();
-        expect(type_detector(uint8clampedarray)).toBe('uint8clampedarray');
+        expect(whoami(uint8clampedarray)).toBe('uint8clampedarray');
       });
 
       it('should work for Int16Array', function () {
         const int16array = new Int16Array();
-        expect(type_detector(int16array)).toBe('int16array');
+        expect(whoami(int16array)).toBe('int16array');
       });
 
       it('should work for Uint16Array', function () {
         const uint16array = new Uint16Array();
-        expect(type_detector(uint16array)).toBe('uint16array');
+        expect(whoami(uint16array)).toBe('uint16array');
       });
 
       it('should work for Int32Array', function () {
         const int32array = new Int32Array();
-        expect(type_detector(int32array)).toBe('int32array');
+        expect(whoami(int32array)).toBe('int32array');
       });
 
       it('should work for Uint32Array', function () {
         const uint32array = new Uint32Array();
-        expect(type_detector(uint32array)).toBe('uint32array');
+        expect(whoami(uint32array)).toBe('uint32array');
       });
 
       it('should work for Float32Array', function () {
         const float32array = new Float32Array();
-        expect(type_detector(float32array)).toBe('float32array');
+        expect(whoami(float32array)).toBe('float32array');
       });
 
       it('should work for Float64Array', function () {
         const float64array = new Float64Array();
-        expect(type_detector(float64array)).toBe('float64array');
+        expect(whoami(float64array)).toBe('float64array');
       });
     });
   });
